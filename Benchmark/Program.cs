eebbb4536be6626e51979aeb96a9ef780499c895
钧元 罗 <k1mlka@outlook.com>
2021-03-14 22:42:45 +0800

    string compare

---
+++ Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Benchmark
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
