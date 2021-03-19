a01bca2f30897ed856b4ce7dc9a3490b1c287481
钧元 罗 <k1mlka@outlook.com>
2021-03-19 21:05:22 +0800

    sqlite

---
+++ MicrosoftSqlitePractice/EhServerGameUserId0To1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace MicrosoftSqlitePractice
+{
+    public class EhServerGameUserId0To1
+    {
+        /// <summary>
+        /// ~~userId 0 is guest, but guest id is 1 in new Users table~~
+        /// Users 0 is ghost
+        /// Users 4 is guest
+        /// 原本的 user 有0，不应有0，全部改为1幽灵。没有问题
+        /// </summary>
+        public EhServerGameUserId0To1()
+        {
+            using var connection = new SqliteConnection("DataSource=C:\\Users\\k1mlka\\Downloads\\ehserver.sqlite");
+            connection.Open();
+
+            var query = connection.CreateCommand();
+            query.CommandText =
+                @"
+                    Update dle_vnr_gamefiles
+                    SET update_user_id=1
+                    WHERE update_user_id=0
+                ";
+            query.ExecuteNonQuery();
+        }
+    }
+}
---
+++ MicrosoftSqlitePractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicrosoftSqlitePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trans = new EhServerTransitionDateTime();
+            trans.Comments2Subtitle();
+            Console.WriteLine("Over");
+        }
+    }
+}
