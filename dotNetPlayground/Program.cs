6eedaed934d25022781876424f79dd4767aa2eca
钧元 罗 <k1mlka@outlook.com>
2021-03-31 18:13:56 +0800

    datetime

--- dotNetPlayground/Program.cs
+++ dotNetPlayground/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace dotNetPlayground
 {
@@ -9,7 +10,9 @@ namespace dotNetPlayground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetFileMd5(@"D:\makura(枕)\サクラノ詩\BGI.exe"));
+            //Console.WriteLine(GetFileMd5(@"D:\makura(枕)\サクラノ詩\BGI.exe"));
+            //TimeTest();
+            //DiffOfDateTimeToUnix();
         }
 
         public static string GetFileMd5(string filePath)
@@ -24,7 +27,22 @@ namespace dotNetPlayground
             {
                 sb.Append(byteItem.ToString("x2"));
             }
+
             return sb.ToString().ToUpper();
+        }
+
+        public static void DateTimeToUnixI18N()
+        {
+            string time = "1970-01-01 00:00:00";
+            DateTime dateTime = Convert.ToDateTime(time);
+            // The key
+            dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+            DateTime zeroDataTime = DateTimeOffset.FromUnixTimeSeconds(0).UtcDateTime;
+            Console.WriteLine(dateTime);
+            Console.WriteLine(zeroDataTime);
+
+            Console.WriteLine(new DateTimeOffset(dateTime).ToUnixTimeSeconds());
+            Console.WriteLine(new DateTimeOffset(zeroDataTime).ToUnixTimeSeconds());
         }
     }
 }
