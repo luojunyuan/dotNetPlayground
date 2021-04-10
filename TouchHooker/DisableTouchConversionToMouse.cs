ffbc3dd93e3edf7e97f319614bec7357cfcf72ad
k1mlka <k1mlka@outlook.com>
2021-04-11 06:25:46 +0800

    fluentmigrator exception

---
+++ TouchHooker/DisableTouchConversionToMouse.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows.Documents;
+
+namespace TouchHooker
+{
+    public class DisableTouchConversionToMouse : IDisposable
+    {
+        static readonly LowLevelMouseProc hookCallback = HookCallback;
+        static IntPtr hookId = IntPtr.Zero;
+
+        public DisableTouchConversionToMouse()
+        {
+            hookId = SetHook(hookCallback);
+        }
+
+        static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            var moduleHandle = UnsafeNativeMethods.GetModuleHandle(null);
+
+            var setHookResult = UnsafeNativeMethods.SetWindowsHookEx(WH_MOUSE_LL, proc, moduleHandle, 0);
+            if (setHookResult == IntPtr.Zero)
+            {
+                throw new Win32Exception();
+            }
+            return setHookResult;
+        }
+
+        delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                var info = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+
+                uint extraInfo = (uint)info.dwExtraInfo.ToInt64();
+                if ((extraInfo & MOUSEEVENTF_FROMTOUCH) == MOUSEEVENTF_FROMTOUCH)
+                {
+                    Trace.WriteLine($"{info.pt.x} {info.pt.y}");
+                    return new IntPtr(1);
+                }
+            }
+
+            return UnsafeNativeMethods.CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        bool disposed;
+
+        public void Dispose()
+        {
+            if (disposed) return;
+
+            UnsafeNativeMethods.UnhookWindowsHookEx(hookId);
+            disposed = true;
+            GC.SuppressFinalize(this);
+        }
+
+        ~DisableTouchConversionToMouse()
+        {
+            Dispose();
+        }
+
+        #region Interop
+
+        // ReSharper disable InconsistentNaming
+        // ReSharper disable MemberCanBePrivate.Local
+        // ReSharper disable FieldCanBeMadeReadOnly.Local
+
+        const uint MOUSEEVENTF_FROMTOUCH = 0xFF515700;
+        const int WH_MOUSE_LL = 14;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct POINT
+        {
+
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        static class UnsafeNativeMethods
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod,
+                uint dwThreadId);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+                IntPtr wParam, IntPtr lParam);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern IntPtr GetModuleHandle(string lpModuleName);
+        }
+
+        // ReSharper restore InconsistentNaming
+        // ReSharper restore FieldCanBeMadeReadOnly.Local
+        // ReSharper restore MemberCanBePrivate.Local
+
+        #endregion
+    }
+}
---
+++ TouchHooker/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace TouchHooker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private readonly HookProc _mouseHook;
+        //private IntPtr _hMouseHook;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            hook = new DisableTouchConversionToMouse();
+        }
+
+        private DisableTouchConversionToMouse hook;
+}
